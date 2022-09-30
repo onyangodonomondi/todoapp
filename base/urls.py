cdce63a41c6e3aff3b99b2d0a4cf8c021c643706
onyangodonomondi <onyangodonomondi@gmail.com>
2022-09-30 18:43:03 +0300

    updates

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from . import views
-from . views import TaskList, TaskDetail , TaskCreate
+from . views import TaskList, TaskDetail , TaskCreate, TaskUpdate, TaskDelete,  CustomLoginView
 
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('',TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
-    path('task-create/', views.TaskCreate.as_view(), name='task-create'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDelete.as_view(), name='task-delete'),
+    path('register/', views.RegisterPage.as_view(), name='register'),
+    
     
     ]
